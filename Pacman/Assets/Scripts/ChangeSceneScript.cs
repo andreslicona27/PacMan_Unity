a1427cad27d5b7d39da3d1dd0cba419ff298fe06
Andres Licona <andreslicona27@icloud.com>
2023-10-16 21:44:37 +0200

    Update2

--- Pacman/Assets/Scripts/ChangeSceneScript.cs
+++ Pacman/Assets/Scripts/ChangeSceneScript.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Class that manage the change of scene in the game 
+/// </summary>
 public class ChangeScene : MonoBehaviour
 {
-  public void LoadScene(string sceneName)
+    /// <summary>
+    /// Function that manage the change of scenes across the game 
+    /// </summary>
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
